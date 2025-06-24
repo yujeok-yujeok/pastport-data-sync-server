20eeb1baaaf644bc80b25a013cd3b6d4eace5b69
dongur2 <holymolyguri@gmail.com>
2025-06-25 01:06:37 +0900

    first commit

---
+++ src/main/java/com/pastport/datasync/DataSyncApplication.java
@@ -0,0 +1,13 @@
+package com.pastport.datasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pastport/datasync/DataSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pastport.datasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
